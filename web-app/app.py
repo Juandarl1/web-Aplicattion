41f66f6815f43995ac60d9f75a23ab84dadce189
Juan <juandavidrodriguezlondono@gmail.com>
2024-05-18 15:16:44 -0500

    import key

---
+++ web-app/app.py
@@ -0,0 +1,6 @@
+from flask import Flask, request, jsonify, render_template
+import opentracing
+
+app=Flask(__Proyecto__)
+
+openai.api_key='sk-proj-IWNvr9t9p6YEgRGNOBjGT3BlbkFJLOY8F05879EQv5lWzVsU'
